fd167daf650b8fb5df6ceaf9d8911bd0ad121c35
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-10-01 23:06:12 +0530

    Add files via upload

---
+++ prophet.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from fbprophet import prophet
+
+
+df = pd.read_csv('dataset.csv')
+df =df[['Date','AveragePrice']].dropna()
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.set_index('Date')
+week_df = df.resample('W').mean()
+w_df = week_df.reset_index().dropna()
+df['Date'] = pd.to_datetime(df['Date'])
+df = df.set_index('Date')
+week_df = df.resample('W').mean()
+d_df = daily_df.reset_index().dropna()
+d_df.columns = ['date', 'High']
+m = Prophet()
+m.fit(d_df)
+future = m.make_future_dataframe(periods=90)
+forecast = m.predict(future)
+forecast[['date', 'Open', 'High', 'Low','Close']].tail()
+fig1 = m.plot(forecast)
